8ae79e58e5efb7088cbdc9d6f793b3c33845e4c3
Eric Terranova <terranova0689@gmail.com>
2019-02-18 14:58:21 -0500

    almost dont with departmentDAL

--- 06-DAO-Exercises/dao-exercises/DAL/DepartmentSqlDAL.cs
+++ 06-DAO-Exercises/dao-exercises/DAL/DepartmentSqlDAL.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using dao_exercises.Models;
+using System.Data.SqlClient;
 
 namespace dao_exercises.DAL
 {
     class DepartmentSqlDAL
     {
+        private const string SQL_GetDepartmentNames = "SELECT * FROM department";
+        private const string SQL_InsertDepartment = @"INSERT INTO department (name) VALUES (@name);";
+        private const string SQL_UpdateDepartment = "UPDATE department SET name = @name;";
         private string connectionString;
 
         // Single Parameter Constructor
@@ -21,7 +25,36 @@ namespace dao_exercises.DAL
         /// <returns></returns>
         public IList<Department> GetDepartments()
         {
-            throw new NotImplementedException();
+            List<Department> output = new List<Department>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = SQL_GetDepartmentNames;
+                    cmd.Connection = conn;
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Department department = new Department();
+
+                        department.Name = Convert.ToString(reader["name"]);
+                        department.Id = Convert.ToInt32(reader["department_id"]);
+
+                        output.Add(department);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+
+            return output;
         }
 
         /// <summary>
@@ -31,7 +64,26 @@ namespace dao_exercises.DAL
         /// <returns>The id of the new department (if successful).</returns>
         public int CreateDepartment(Department newDepartment)
         {
-            throw new NotImplementedException();
+            int count = 0;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(SQL_InsertDepartment, conn);
+
+                    cmd.Parameters.AddWithValue("@name", newDepartment.Name);
+
+                    count = newDepartment.Id;
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+
+            return count;
         }
 
         /// <summary>
@@ -41,7 +93,31 @@ namespace dao_exercises.DAL
         /// <returns>True, if successful.</returns>
         public bool UpdateDepartment(Department updatedDepartment)
         {
-            throw new NotImplementedException();
+            bool result = false;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(SQL_UpdateDepartment, conn);
+
+                    cmd.Parameters.AddWithValue("@name", updatedDepartment.Name);
+
+                    int count = cmd.ExecuteNonQuery();
+
+                    if (count>0)
+                    {
+                        result = true;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+
+                throw;
+            }
+
+            return result;
         }
 
     }
